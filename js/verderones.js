260e6a876d4d32fe22aee04ec4d27ee4b86d7f65
Patri <patriciaaguayop@gmail.com>
2024-04-11 17:44:41 +0200

    Falta ver como añadir un deslizamiento suave hacia las distintas secciones

---
+++ js/verderones.js
@@ -0,0 +1,23 @@
+"Use strict";
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                const offsetTop = targetElement.offsetTop;
+                
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: "smooth" // Hace que el desplazamiento sea suave
+                });
+            }
+        });
+    });
+});*/
